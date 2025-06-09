67d9a5ead7bd8b4cbc3952b8d1dca39c852ac399
Sebastian Zib <e11907072@student.tuwien.ac.at>
2025-06-09 16:16:26 +0200

    added generation of entity nodes and relationship nodes

--- packages/webview/src/model.ts
+++ packages/webview/src/model.ts
@@ -1,3 +1,96 @@
+import { DiamondNode, EdgePlacement, PreRenderedElement, RectangularNode, SLabelImpl } from 'sprotty';
 import { LibavoidEdge } from 'sprotty-routing-libavoid';
 
-export class RelationshipEdge extends LibavoidEdge {}
+
+
+/**
+ * node representing an entity
+ */
+export class EntityNode extends RectangularNode {
+    expanded: boolean;
+    weak: boolean;
+    isUml: boolean;
+}
+
+/**
+ * node representing a relationship
+ */
+export class RelationshipNode extends DiamondNode {
+    weak: boolean;
+}
+
+/**
+ * edge representing a link between two nodes
+ */
+export class NotationEdge extends LibavoidEdge {
+    isSource: boolean;
+    notation: string;
+    connectivity: string;
+    relationshipType: string;;
+}
+
+/**
+ * label representing the cardinality of a link between nodes
+*/
+export class CardinalityLabel extends SLabelImpl {
+    override edgePlacement = <EdgePlacement> {
+        position: 0.5,
+        side: 'top',
+        rotate: false,
+        offset: 10
+    };
+}
+
+export class LeftCardinalityLabel extends SLabelImpl {
+    override edgePlacement = <EdgePlacement> {
+        position: 0.2,
+        side: 'top',
+        rotate: false,
+        offset: 10
+    };
+}
+
+export class RightCardinalityLabel extends SLabelImpl {
+    override edgePlacement = <EdgePlacement> {
+        position: 0.8,
+        side: 'top',
+        rotate: false,
+        offset: 10
+    };
+}
+
+export class RoleLabel extends SLabelImpl {
+    override edgePlacement = <EdgePlacement> {
+        position: 0.5,
+        side: 'bottom',
+        rotate: false,
+        offset: 10
+    };
+}
+
+export class LeftRoleLabel extends SLabelImpl {
+    override edgePlacement = <EdgePlacement> {
+        position: 0.2,
+        side: 'bottom',
+        rotate: false,
+        offset: 10
+    };
+}
+
+export class RightRoleLabel extends SLabelImpl {
+    override edgePlacement = <EdgePlacement> {
+        position: 0.8,
+        side: 'bottom',
+        rotate: false,
+        offset: 10
+    };
+}
+
+export class InheritanceEdge extends LibavoidEdge {
+
+}
+
+export class PopupButton extends PreRenderedElement {
+    target: string;
+	kind: string;
+}
